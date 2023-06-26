3e0ba7fa1ff3736818af1cb148e56d49f4349803
yanghaodong0904 <yang18406576321@163.com>
2023-06-26 21:58:11 +0800

    新增test01的功能

---
+++ test01.py
@@ -0,0 +1,42 @@
+"""
+功能描述：
+创建人：杨浩栋
+日期：
+实现逻辑：
+导包：
+    -1
+    -2
+    -3
+
+"""
+# 函数
+# def fun1(a,b):
+#
+#     return (a + b)
+# sumab = fun1(1,3)
+# print(sumab)
+
+# dict1 = {"name":"杨浩栋","age":"20"}
+#
+#
+# j = str(input(":"))
+# if j in dict1.values():
+#     print("存在")
+# else:
+#     i = str(input(":"))
+#
+#     if i not in dict1:
+#         dict1[i] = j
+#         # print(dict1)
+# print(dict1)
+
+# 文件读写
+# day_3file = open("text.txt","r",encoding="utf-8")
+# # day_3file.write("新创建的内容")
+# print(day_3file.read())
+# # day_3file.close()
+
+import os
+# # 重命名
+# os.rename("text.txt","text1729.txt")
+print(os.path.dirname(__file__))
